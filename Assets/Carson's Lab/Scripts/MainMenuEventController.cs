8f2ee8e2a9c45c83743c403f2f3bf57de62c1c15
Carson Ivory <ckivory@gmail.com>
2020-10-11 21:44:51 -0600

    Main Menu, basic Options menu, Exit button, and stub of portfolio.

---
+++ Assets/Carson's Lab/Scripts/MainMenuEventController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuEventController : MonoBehaviour
+{
+    public void StartButtonPushed()
+    {
+        SceneManager.LoadScene("Portfolio");
+    }
+
+    public void OptionsButtonPushed()
+    {
+        SceneManager.LoadScene("Options Menu");
+    }
+
+    public void ExitButtonPushed()
+    {
+        #if UNITY_EDITOR
+            // Application.Quit() does not work in the editor so
+            // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
