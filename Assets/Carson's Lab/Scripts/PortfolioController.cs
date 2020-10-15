1cde18402b03761a5986ba0075ae756adb58f444
ckivory <ckivory@gmail.com>
2020-10-15 11:59:45 -0600

    Added basic ability to draw tabs. TODO: Integrate a tab with a window so they appear to be one seamless whole.

---
+++ Assets/Carson's Lab/Scripts/PortfolioController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortfolioController : MonoBehaviour
+{
+    // TODO: Using a list of available tabs, display them all at the top of the portfolio in order without Z-conflicts
+    public List<TabController> tabs;
+
+    // TODO: Using a list of windows, display one at a time depending on which tab was most recently clicked
+    public List<WindowController> windows;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Carson's Lab/Scripts/TabController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabController : MonoBehaviour
+{
+    public Image neck;
+    public Image headBorder;
+    public Image headFace;
+
+    // TODO: Maybe move this value to some globals game-variables script so it can be reused. Especially if I want to save data between scenes.
+    public float border_width;
+    public float tab_height;
+
+    public void ConstructTab(float window_width, int num_tabs, int tab_index)
+    {
+        neck.transform.localPosition = Vector2.zero;
+        neck.GetComponent<RectTransform>().sizeDelta = new Vector2(window_width, neck.sprite.border[3]);
+
+        float tab_width = (window_width * 1f / num_tabs);
+        // Left side of window, plus at least half a tab width to center it, offset by the size of the other tabs
+        Vector2 head_position = new Vector2((-1 * window_width / 2) + tab_width * (0.5f + tab_index), tab_height / 2 - neck.sprite.border[3] / 2);
+        headBorder.transform.localPosition = head_position;
+
+        Vector2 head_size = new Vector2(tab_width, tab_height);
+        headBorder.GetComponent<RectTransform>().sizeDelta = head_size;
+
+        headFace.transform.localPosition = new Vector2(head_position.x, head_position.y - border_width / 2);
+        headFace.GetComponent<RectTransform>().sizeDelta = new Vector2(head_size.x - border_width * 2, head_size.y - border_width);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Makes the tab think it belongs in the middle of a 300-unit-wide window with 3 tabs, which should make it 100 units wide and centered at X=0
+        ConstructTab(300f, 3, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
