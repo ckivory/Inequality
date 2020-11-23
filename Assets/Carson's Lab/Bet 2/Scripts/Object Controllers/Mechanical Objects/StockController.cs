58aa89b57bb849be3728f9f385395b5b5eebd87d
ckivory <ckivory@gmail.com>
2020-11-22 17:20:57 -0700

    Added stub for stock controller.

---
+++ Assets/Carson's Lab/Bet 2/Scripts/Object Controllers/Mechanical Objects/StockController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StockController : MonoBehaviour
+{
+    // Make arrow buttons increase and decrease stock amount
+    // Make buy and sell buttons get grayed out if you don't have enough money to buy or enough stock to sell
+    // Make buy and sell buttons actually buy and sell
+    // Make stock change value after each round
+    // Make stock payout happen after every other round
+}
