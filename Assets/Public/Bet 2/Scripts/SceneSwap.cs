702510ba80e0b79071b876123cae563a9eb103e3
kiyyou <kim.you8295@gmail.com>
2020-12-11 19:33:32 -0700

    Updated UI for Main screen
    waiting on sera for final background

---
+++ Assets/Public/Bet 2/Scripts/SceneSwap.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Object.DontDestroyOnLoad example.
+//
+// Two scenes call each other. This happens when OnGUI button is clicked.
+// scene1 will load scene2; scene2 will load scene1. Both scenes have
+// the Menu GameObject with the SceneSwap.cs script attached.
+//
+// AudioSource plays an AudioClip as the game runs. This is on the
+// BackgroundMusic GameObject which has a music tag.  The audio
+// starts in AudioSource.playOnAwake. The DontDestroy.cs script
+// is attached to BackgroundMusic.
+
+public class SceneSwap : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        int xCenter = (Screen.width / 2);
+        int yCenter = (Screen.height / 2);
+        int width = 400;
+        int height = 120;
+
+        GUIStyle fontSize = new GUIStyle(GUI.skin.GetStyle("button"));
+        fontSize.fontSize = 32;
+
+        Scene scene = SceneManager.GetActiveScene();
+
+        if (scene.name == "scene1")
+        {
+            // Show a button to allow scene2 to be switched to.
+            if (GUI.Button(new Rect(xCenter - width / 2, yCenter - height / 2, width, height), "Load second scene", fontSize))
+            {
+                SceneManager.LoadScene("scene2");
+            }
+        }
+        else
+        {
+            // Show a button to allow scene1 to be returned to.
+            if (GUI.Button(new Rect(xCenter - width / 2, yCenter - height / 2, width, height), "Return to first scene", fontSize))
+            {
+                SceneManager.LoadScene("scene1");
+            }
+        }
+    }
+}
